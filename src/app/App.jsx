4b23c1f59455af3e430e4a784635b54e9e7442d2
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-23 06:35:01 -0500

    Implement basic routing

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './../pages/home/Home.jsx';
+import Product from './../pages/product/Product.jsx';
+import Pricing from './../pages/pricing/Pricing.jsx'
+import PageNotFound from "../pages/not_found/PageNotFound.jsx";
+
 
 export default function App() {
 
     return (
-        <div>
-            <h1>vite-worldwise</h1>
-        </div>
+        <BrowserRouter>
+            <Routes>
+                <Route>
+                    <Route path="/" element={<Home />} />
+                    <Route path="product" element={<Product />} />
+                    <Route path="pricing" element={<Pricing />} />
+                    <Route path="*" element={<PageNotFound />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
 
     );
 };
