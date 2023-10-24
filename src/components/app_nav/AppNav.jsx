daf2891fb7382306a4f5ad6bb40eed59e747e102
MAbdurahman <mdbdrrhm@gmail.com>
2023-10-24 08:43:55 -0500

    Build a couple of pages

---
+++ src/components/app_nav/AppNav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './AppNav.module.css';
+
+export default function AppNav() {
+
+    return (
+        <nav className={styles.nav}>
+            <h2>AppNav</h2>
+        </nav>
+
+    );
+};
---
+++ src/pages/app_layout/AppLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './AppLayout.module.css'
+
+export default function AppLayout() {
+
+    return (
+        <div className={styles.app}>
+            <h2>AppLayout</h2>
+        </div>
+
+    );
+};
